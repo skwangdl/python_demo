69e127e86cd5024a6fae699e576b7397323d8b50
wangck4 <wangck4@lenovo.com>
2019-04-02 18:25:52 +0800

    kepler add demo format method fix

--- 2_formatMethod.py
+++ 2_formatMethod.py
@@ -5,5 +5,20 @@ def demoFormatMethod():
     name = 'hello kepler'
     print('{0}, spring , {1}'.format(age, name))
 
+def demoFormatMethod_double():
+    age = 1.0/3
+    print('{0:.3f}'.format(age))
+
+def demoFormatMethod_str():
+    name = 'hello, my name is kepler'
+    print('{0:_^11}'.format(name))
+
+def demoFormatMethod_strWithKey():
+    name = '{name} is {my}'
+    print(name.format(name='kepler', my='my'))
+
 if __name__ == '__main__':
     demoFormatMethod()
+    demoFormatMethod_double()
+    demoFormatMethod_str()
+    demoFormatMethod_strWithKey()
