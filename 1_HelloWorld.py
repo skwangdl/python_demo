053be037341f3e64b998ce0298ff0ecbb7eb265b
wangck4 <wangck4@lenovo.com>
2019-04-02 18:17:12 +0800

    kepler add demo format method file

--- 1_HelloWorld.py
+++ 1_HelloWorld.py
@@ -1,2 +1,7 @@
 
-print("hello kepler")
+# this is first demo about python
+def helloWorld():
+    print("hello kepler")
+
+if __name__ == '__main__':
+    helloWorld()
---
+++ 2_formatMethod.py
@@ -0,0 +1,9 @@
+
+# format method in python
+def demoFormatMethod():
+    age = 20
+    name = 'hello kepler'
+    print('{0}, spring , {1}'.format(age, name))
+
+if __name__ == '__main__':
+    demoFormatMethod()
