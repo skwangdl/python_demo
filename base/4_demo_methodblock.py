627bb1ed1de3fc3f35ddf8deda4b7510e4d1c811
wangck4 <wangck4@lenovo.com>
2019-04-05 21:58:11 +0800

    SK fix

--- base/4_demo_methodblock.py
+++ base/4_demo_methodblock.py
@@ -6,6 +6,15 @@ def func(x):
     x = 2
     print('Change local x to ', x)
 
+def func_global():
+    global x
+    print('x is', x)
+    x = 3
+    print('change global x is', x)
+
+
 if __name__ == '__main__':
     func(x)
     print('x is still', x)
+    func_global()
+    print('change global x is', x)
