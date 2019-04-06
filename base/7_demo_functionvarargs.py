ad3ce4ca9e9c9e2decc7998497d60ec4b146cc95
wangck4 <wangck4@lenovo.com>
2019-04-06 21:50:30 +0800

    SK fix

---
+++ base/7_demo_functionvarargs.py
@@ -0,0 +1,10 @@
+
+def total(a=5, *numbers, **phonebook):
+    print('a', a)
+    for item in numbers:
+        print("numbers:", numbers)
+
+    for first_part, second_part in phonebook.items():
+        print(first_part, second_part)
+
+    
