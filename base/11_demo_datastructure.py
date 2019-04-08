2b0f9cd93378390a3bc18dcf9b477dc142a4e01f
wangck4 <wangck4@lenovo.com>
2019-04-08 20:15:10 +0800

    SK fix

--- base/11_demo_datastructure.py
+++ base/11_demo_datastructure.py
@@ -1,6 +1,6 @@
 
 # demo about list in python
-def method():
+def demo_list():
     shoplist = ['apple', 'mango', 'carrot', 'banana']
     print('I have ', len(shoplist), 'items to purchase')
     print('These items are: ', end=' ')
@@ -13,5 +13,49 @@ def method():
     shoplist.sort()
     print('My shopping is now: ', shoplist)
 
+def demo_tuple():
+    zoo = ('python', 'java', 'go')
+    print(len(zoo))
+    new_zoo = 'ruby', 'csharp', zoo
+    print(len(new_zoo))
+    print(new_zoo[2][2])
+
+
+def demo_map():
+    map = {'key1': 'value1', 'key2': 'value2'}
+    print('key1 value: ', map['key1'])
+
+    for name, address in map.items():
+        print('contact {} at {}'.format(name, address))
+
+def demo_sequence():
+    list = ['a', 'b', 'c', 'd', 'e']
+    print('item 1 to 3 is', list[1:3])
+
+def demo_set():
+    bir = set(['a', 'b', 'c', 'd'])
+    birc = set(['a', 'b', 'c', 'e'])
+    print(birc & bir)
+    print(birc | bir)
+    print(birc ^ bir)
+
+def demo_str():
+    name = 'kepler'
+    if name.startswith('k'):
+        print('startswith k')
+    if 'p' in name:
+        print('contain p')
+    if name.find('er') != -1:
+        print('contain er')
+    delimiter = '_*_'
+    templist = ['a', 'b', 'c', 'd']
+    print(delimiter.join(templist))
+
+
 if __name__ == '__main__':
-    method()
+    demo_list()
+    demo_tuple()
+    demo_map()
+    demo_sequence()
+    demo_set()
+    demo_str()
