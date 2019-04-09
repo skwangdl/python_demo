e21a7b7c73f189b5ea6cc33697dce44f8fd3a208
wangck4 <wangck4@lenovo.com>
2019-04-09 20:00:23 +0800

    SK fix

--- base/16_demo_file.py
+++ base/16_demo_file.py
@@ -5,6 +5,23 @@ def reverse(text):
 def is_palindrome(text):
     return text == reverse(text)
 
+def demo_openfile():
+    poem = '''\
+    hello
+    kepler
+    '''
+    f = open('D:\\temp\\pome.txt', 'w')
+    f.write(poem)
+    f.close()
+    f = open('D:\\temp\\pome.txt')
+    while True:
+        line = f.readline()
+        if len(line) == 0:
+            break
+        print(line, end=' ')
+    f.close()
+
 if __name__ == '__main__':
     msg = "abccba"
     print(is_palindrome(msg))
+    demo_openfile()
