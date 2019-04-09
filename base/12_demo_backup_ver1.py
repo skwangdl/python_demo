437b818a1c20c966f02564970bb98241437a1cf5
wangck4 <wangck4@lenovo.com>
2019-04-09 17:37:26 +0800

    SK fix

--- base/12_demo_backup_ver1.py
+++ base/12_demo_backup_ver1.py
@@ -1,8 +1,22 @@
 import os
 import time
 
-def temp_method():
-    source = ['/Users/swa/notes']
-    target_dir = '/Users/swa/backup'
+# demo about run command in enviroment using python
+def backup():
+    source = ['"D:\\temp\\temp_source"']
+    target_dir = 'D:\\temp\\temp_target'
     target = target_dir + os.sep + \
-            time.strftime("")
+             time.strftime('%Y%m%d%H%M%S') + '.zip'
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
+    print('Zip command is:')
+    print(zip_command)
+    print('Running:')
+    if os.system(zip_command) == 0:
+        print('Successful backup to', target)
+    else:
+        print('Backup failed')
+
+if __name__ == '__main__':
+    backup()
