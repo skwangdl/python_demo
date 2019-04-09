db1e7bf136337449c36630c626413ece88954b39
wangck4 <wangck4@lenovo.com>
2019-04-09 10:25:41 +0800

    SK fix

---
+++ base/12_demo_backup_ver1.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+def temp_method():
+    source = ['/Users/swa/notes']
+    target_dir = '/Users/swa/backup'
+    target = target_dir + os.sep + \
+            time.strftime("")
