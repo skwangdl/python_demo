2ccd83879af0af2fb82c5a42ca219f74f128f50d
wangck4 <wangck4@lenovo.com>
2019-04-10 19:52:12 +0800

    SK fix

--- base/25_demo_decorator.py
+++ base/25_demo_decorator.py
@@ -1,18 +1,23 @@
 
-def debug(func):
-    def wrapper():
-        print('[DEBUG]: enter {}()'.format(func.__name__))
-        return func()
-    return wrapper()
+# TODO
+class logging(object):
+    def __init__(self, level):
+        self.level = level
 
-@debug
-def say_hello():
-    print('say hello')
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            print("[{level}]: enter function {func}()".format(level=self.level, func=func.__name__))
+            func(*args, **kwargs)
+        return wrapper
 
-@debug
-def say_goodbye():
-    print('say hello')
+@logging(level='DEBUG')
+def say_hello(str):
+    print('say hello {}'.format(str))
+
+@logging(level='INFO')
+def say_goodbye(str):
+    print('say goodbye {}'.format(str))
 
 if __name__ == '__main__':
-    say_hello
-    say_goodbye
+    say_hello('kepler')
+    say_goodbye('wsk')
