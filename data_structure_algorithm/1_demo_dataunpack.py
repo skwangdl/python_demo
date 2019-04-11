4e372114a3734a31e1fdc55a72ecfb7590c51cc6
wangck4 <wangck4@lenovo.com>
2019-04-11 18:11:46 +0800

    SK fix

---
+++ data_structure_algorithm/1_demo_dataunpack.py
@@ -0,0 +1,14 @@
+
+def demo_unpack():
+    data = ['ACE', 50, 91.1, (2019, 1, 2)]
+    a, b, c, d = data
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    str = 'kepler'
+    e,f,g,h,i,j = str
+    print('{} {} {} {} {} {}'.format(e,f,g,h,i,j))
+
+if __name__ == '__main__':
+    demo_unpack()
