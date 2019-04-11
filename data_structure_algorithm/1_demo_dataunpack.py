599ad06f5a1f005eb3c43150dd7bd178a4540f61
wangck4 <wangck4@lenovo.com>
2019-04-11 18:20:43 +0800

    SK fix

--- data_structure_algorithm/1_demo_dataunpack.py
+++ data_structure_algorithm/1_demo_dataunpack.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def demo_unpack():
     data = ['ACE', 50, 91.1, (2019, 1, 2)]
@@ -10,5 +11,12 @@ def demo_unpack():
     e,f,g,h,i,j = str
     print('{} {} {} {} {} {}'.format(e,f,g,h,i,j))
 
+def drop_first_last(grades):
+    first, *middle, last = grades
+    return np.mean(middle)
+
 if __name__ == '__main__':
     demo_unpack()
+    gradles = (100,2,3,4,5,6,80)
+    avg = drop_first_last(grades=gradles)
+    print(avg)
