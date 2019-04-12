9e40b5b6f37b9d337af1951991dff0bab611edb9
wangck4 <wangck4@lenovo.com>
2019-04-12 10:08:45 +0800

    SK fix

---
+++ data_structure_algorithm/3_demo_collections.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+# 在迭代操作或者其他操作的时候，怎样只保留最后d有限几个元素的历史记录
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+if __name__ == '__main__':
+    with open(r'D:\\Temp\\pome.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-' * 20)
