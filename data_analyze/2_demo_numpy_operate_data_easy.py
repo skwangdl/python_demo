647e718c7e5ef0de47d87350aadbaeca8c6d6e58
wangck4 <wangck4@lenovo.com>
2019-04-13 21:30:15 +0800

    SK fix

--- data_analyze/2_demo_numpy_operate_data_easy.py
+++ data_analyze/2_demo_numpy_operate_data_easy.py
@@ -1,3 +1,31 @@
 import numpy as np
 
-print(np.__version__)
+def print_version():
+    print(np.__version__)
+
+def create_array():
+    arr = np.arange(10)
+    print(arr)
+
+def create_array_all_true():
+    arr = np.ones((3,3), dtype=bool)
+    print(arr)
+
+def get_odd_number():
+    arr = np.array([0,1,2,3,4,5,6,7,8,9])
+    arr = arr[arr % 2 == 1]
+    print(arr)
+
+def np_where_demo():
+    arr = np.array([1,2,3,4,5,6,7,8,9,10])
+    out = np.where(arr % 2 == 1, -1, arr)
+    print(out)
+    print(arr)
+
+
+if __name__ == '__main__':
+    print_version()
+    create_array()
+    create_array_all_true()
+    get_odd_number()
+    np_where_demo()
---
+++ data_analyze/3_demo_numpy_operate_data_complex.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def demo():
+    print()
+
+if __name__ == '__main__':
+    demo()
