b7ed6d13282095a45143d5327eb9a24c6258785f
wangck4 <wangck4@lenovo.com>
2019-04-24 11:51:35 +0800

    SK fix

--- data_analyze/7_demo_pandas_IO.py
+++ data_analyze/7_demo_pandas_IO.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 def demo_pandas_io():
---
+++ data_analyze/8_demo_pandas_profiling.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def demo_pandas_profiling():
+    print()
+
+if __name__ == '__main__':
+    demo_pandas_profiling()
