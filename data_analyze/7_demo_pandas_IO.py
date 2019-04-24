83555d9a2bbeca3006513c488a7a5481401c4819
wangck4 <wangck4@lenovo.com>
2019-04-24 11:33:55 +0800

    SK fix

--- data_analyze/7_demo_pandas_IO.py
+++ data_analyze/7_demo_pandas_IO.py
@@ -4,7 +4,12 @@ import pandas as pd
 def demo_pandas_io():
     df = pd.read_csv("data/test.csv", index_col=['PassengerId'])
     print(df)
-
+    tips = df[['Pclass', 'Name']].head(5)
+    print(tips)
+    rs = tips[tips['Pclass'] == 2]
+    print(rs)
+    rs = tips.groupby('Pclass').size()
+    print(rs)
 
 if __name__ == '__main__':
     demo_pandas_io()
