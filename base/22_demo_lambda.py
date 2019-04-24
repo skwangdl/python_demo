565a525606a233ddf767876ee737efe708e5a2fe
wangck4 <wangck4@lenovo.com>
2019-04-24 17:33:15 +0800

    SK fix

--- base/22_demo_lambda.py
+++ base/22_demo_lambda.py
@@ -15,9 +15,19 @@ def lambda_sort():
 def lambda_reduce():
     return reduce(lambda a, b:'{} {}'.format(a,b), [1,2,3])
 
+def lambda_demo():
+    return str2int(list('123'))
+
+def str2int(s):
+    CHAR_TO_INT = {'0': 0,'1': 1,'2': 2,'3': 3,'4': 4,'5': 5,
+        '6': 6,'7': 7,'8': 8,'9': 9}
+    ints = map(lambda ch: CHAR_TO_INT[ch], s)
+    return reduce(lambda x, y: x * 10 + y, ints)
+
 if __name__ == '__main__':
     print(lambda_list_1())
     print(lambda_list_2())
     print(lambda_map())
     print(lambda_sort())
     print(lambda_reduce())
+    print(lambda_demo())
---
+++ data_analyze/9_demo_pandas_missingno.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def demo():
+    print()
+
+if __name__ == '__main__':
+    demo()
