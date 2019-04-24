dc59694373e9cad27cbf36b4b3e919630a4d0daa
wangck4 <wangck4@lenovo.com>
2019-04-24 13:30:19 +0800

    SK fix

--- data_analyze/8_demo_pandas_profiling.py
+++ data_analyze/8_demo_pandas_profiling.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
+import pandas_profiling as pfr
 
 def demo_pandas_profiling():
-    print()
+    df = pd.read_csv("data/tcdata.csv")
+    result = pfr.ProfileReport(df)
+    result.to_file("data/report.html")
 
 if __name__ == '__main__':
     demo_pandas_profiling()
