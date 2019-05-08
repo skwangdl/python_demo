cb2f7105781173e133847de4d3a81691089fda10
wangck4 <wangck4@lenovo.com>
2019-05-08 19:11:04 +0800

    SK fix

---
+++ base/60_demo_GUI_Tkinter.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='hello kepler')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+if __name__ == '__main__':
+    app = Application()
+    app.master.title('Hello world')
+    app.mainloop()
