b5da3f1d63bf2d9af0ccbb2c22ed665801ae7e21
wangck4 <wangck4@lenovo.com>
2019-05-09 20:30:46 +0800

    SK fix

---
+++ base/68_demo_AIO_asyncio.py
@@ -0,0 +1,13 @@
+import asyncio
+import threading
+
+@asyncio.coroutine
+def hello():
+    print('hello world (%s)' % threading.current_thread())
+    yield from asyncio.sleep(1)
+    print('hello again (%s)' % threading.current_thread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
