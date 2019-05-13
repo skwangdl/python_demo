85b8acc67ec8ea7176b3e17a9a0bfe9bf6d75152
wangck4 <wangck4@lenovo.com>
2019-05-13 15:26:38 +0800

    SK fix

--- awesome_python3_webapp/www/config_default.py
+++ awesome_python3_webapp/www/config_default.py
@@ -13,7 +13,7 @@ configs = {
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': 'kepler',
+        'password': 'Kepler',
         'db': 'awesome'
     },
     'session': {
