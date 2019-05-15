ea66a3fde6c559aeebda089608c16b19ca508da4
wangck4 <wangck4@lenovo.com>
2019-05-15 17:17:55 +0800

    SK fix

--- base/57_demo_requests.py
+++ base/57_demo_requests.py
@@ -1,9 +1,16 @@
 import requests
 
 def demo_requests():
-    r = requests.get('https://www.baidu.com')
-    print(r.headers)
-    print(r.content)
+    url = "http://xportal-core-service.earth.xpaas.lenovo.com/deploy/resource-bundle"
+    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"file\"; filename=\"C:\\Users\\issuser\\Desktop\\AutotestReport.zip\"\r\nContent-Type: application/zip\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+    headers = {
+        'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        'Authorization': "Bearer 5ebf2d74-71c1-49f2-b9fd-13a7bf7252eb",
+        'cache-control': "no-cache",
+        'Postman-Token': "c8a7cbe2-b9d4-4a2d-b09d-c1bd3e1efb24"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
 
 if __name__ == '__main__':
     demo_requests()
--- data_analyze/9_demo_pandas_missingno.py
+++ data_analyze/9_demo_pandas_missingno.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def demo():
+def demo_isnull_notnull_calculator():
     df = pd.DataFrame(np.random.randn(5,3), index=['a','c','e','f','g'], columns=['one','two','three'])
     df = df.reindex(['a','b','c','d','e','f','g'])
     print(df)
@@ -9,6 +9,30 @@ def demo():
     print(df['a':'c'].isnull())
     print(df['one'].notnull())
     print(df['a':'c'].notnull())
+    # Nan不被当做0处理，自动跳过
+    print(df['one'].sum())
+
+def demo_all_nan():
+    # 计算时遇到Nan时跳过， Nan相加为Nan
+    df = pd.DataFrame(index=[1,2,3,4,5], columns=['one', 'two'])
+    print(df)
+    print(df['one'].sum())
+
+def demo_fillna():
+    df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f', 'g'], columns=['one', 'two', 'three'])
+    df = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+    print(df)
+    print(df.fillna(0))
+
+def demo_pad_fill_back_fill():
+    df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f', 'g'], columns=['one', 'two', 'three'])
+    df = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+    print(df)
+    print(df.fillna(method='pad'))
+    print(df.fillna(method='bfill'))
 
 if __name__ == '__main__':
-    demo()
+    # demo_isnull_notnull_calculator()
+    # demo_all_nan()
+    # demo_fillna()
+    demo_pad_fill_back_fill()
