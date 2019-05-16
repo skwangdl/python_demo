ec27412de4e940eb25f312fb27ff0d22fbc6f99e
wangck4 <wangck4@lenovo.com>
2019-05-16 14:11:16 +0800

    SK fix

--- data_analyze/10_demo_matplotlib.py
+++ data_analyze/10_demo_matplotlib.py
@@ -1,4 +1,5 @@
 from matplotlib.pyplot import *
+import numpy as np
 
 def demo_matplotlib():
     x = [1,2,3,4,5,6]
@@ -26,5 +27,38 @@ def demo_matplotlib():
     scatter(x,y)
     show()
 
+def demo_hist():
+    figure()
+    dataset = [1,2,30,4,5,54,2,45,12,23,5,9,6,3,2,12,56,73,2]
+    subplot(1,2,1)
+    boxplot(dataset, vert=False)
+    subplot(1,2,2)
+    # 直方图
+    hist(dataset)
+    show()
+
+def demo_sin_cos():
+    x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+    y = np.cos(x)
+    y1 = np.sin(x)
+    plot(x,y)
+    plot(x,y1)
+    # 图表名称
+    title('Functions $\sin$ and $\cos$')
+    # x,y轴坐标范围
+    xlim(-3,3)
+    ylim(-1,1)
+    # 坐标上刻度
+    xticks([-np.pi, -np.pi/2.0, np.pi/2, np.pi],
+           [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+    yticks([-1,0,1],
+           [r'$-1$', r'$0$', r'$+1$'])
+    # 网格
+    grid()
+    show()
+
+
 if __name__ == '__main__':
-    demo_matplotlib()
+    # demo_matplotlib()
+    # demo_hist()
+    demo_sin_cos()
