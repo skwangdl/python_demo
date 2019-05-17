43b8ce7b45c7a32ccc033aa350fce70dc1b07def
wangck4 <wangck4@lenovo.com>
2019-05-17 18:05:07 +0800

    SK fix

--- data_analyze/14_demo_get_data_work.py
+++ data_analyze/14_demo_get_data_work.py
@@ -14,6 +14,7 @@ from sklearn.externals import joblib
 import numpy as np
 
 def demo_make_model():
+    # 数据集前9列为X，为监测数据， 10列为Y，为结果，通过X预测Y
     raw_data = "data/dcdata.data"
     # 加载数据集
     dataset = np.loadtxt(raw_data, delimiter=",")
@@ -28,20 +29,27 @@ def demo_make_model():
     Y_test = dataset[500:, 8]
 
     print('\n调用scikit的tree.DecisionTreeClassifier()')
+    # 设定决策树叶子节点为2个
     model = tree.DecisionTreeClassifier(min_samples_leaf=2)
     start_time = time.time()
+    # 添加训练数据，生成model预测模型，构建X与Y的映射关系
     model.fit(X_train, Y_train)
     print('training took %fs!' % (time.time() -  start_time))
+    # 将预测模型持久化为文件,模型为决策树
     joblib.dump(value=model, filename='data/Decisiontree.model')
+    # 通过model预测，期望得到的值
     expected = Y_test
+    # 通过model预测，实际得到的值
     predicted = model.predict(X_test)
     print(metrics.confusion_matrix(expected, predicted))
+    # 生成报告
     print(metrics.classification_report(expected, predicted))
 
 def demo_test_model():
     dataset = np.loadtxt(fname="data/dcdata.data", delimiter=",")
     x_predict = dataset[0:10, 0:8]
     y_real = dataset[0:10, 8]
+    #从文件获取决策树模型
     gnbmodel = joblib.load(filename="data/Decisiontree.model")
     y_predict = gnbmodel.predict(x_predict)
     print('预测值：')
@@ -50,5 +58,5 @@ def demo_test_model():
     print(y_real)
 
 if __name__ == '__main__':
-    # demo_make_model()
-    demo_test_model()
+    demo_make_model()
+    # demo_test_model()
